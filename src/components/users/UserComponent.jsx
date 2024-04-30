bc772771666fd232e89343fae6c6a7a83741feb7
priya-prasanth <priyaprasanths168.com>
2024-04-30 11:25:20 +0530

    img imported

--- src/components/users/UserComponent.jsx
+++ src/components/users/UserComponent.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { listUser } from "../../Redux/Actions/userActions";
 import Message from "../LoadingError/Error";
 import Loading from "../LoadingError/Loading";
+import avatarimg from "../../assets/images/avatar.png";
 
 const UserComponent = () => {
   const dispatch = useDispatch();
@@ -61,36 +62,33 @@ const UserComponent = () => {
             <Message variant="alert-danger">{error}</Message>
           ) : (
             <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                  {
-                    users.map(
-                (user => (
-                  <div className="col" key={user._id}>
-                    <div className="card card-user shadow-sm">
-                      <div className="card-header">
-                        <img
-                          className="img-md img-avatar"
-                          src="src/assets/images/avatar.png"
-                          alt="User pic"
-                        />
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title mt-5">{user.name}</h5>
-                        <div className="card-text text-muted">
-                          {user.isAdmin === true ? (
-                            <p className="m-0">Admin</p>
-                          ) : (
-                              <p className="m-0">Customer</p>
-                          )}
-                          
-                          <p>
-                            <a href={`mailto:${user.email}`}>{user.email}</a>
-                          </p>
-                        </div>
+              {users.map((user) => (
+                <div className="col" key={user._id}>
+                  <div className="card card-user shadow-sm">
+                    <div className="card-header">
+                      <img
+                        className="img-md img-avatar"
+                        src={avatarimg}
+                        alt="User pic"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title mt-5">{user.name}</h5>
+                      <div className="card-text text-muted">
+                        {user.isAdmin === true ? (
+                          <p className="m-0">Admin</p>
+                        ) : (
+                          <p className="m-0">Customer</p>
+                        )}
+
+                        <p>
+                          <a href={`mailto:${user.email}`}>{user.email}</a>
+                        </p>
                       </div>
                     </div>
                   </div>
-                ))
-              )}
+                </div>
+              ))}
             </div>
           )}
 
