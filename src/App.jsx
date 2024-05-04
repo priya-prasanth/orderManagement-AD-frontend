8d3e7495a385a4ba8f2871dc7f532345d33ce1d4
priya-prasanth <priyaprasanths168.com>
2024-05-04 12:53:19 +0530

    privateRouter added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { store } from "./Redux/store.js";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/productScreen.jsx";
 import OrderScreen from "./screens/OrderScreen.jsx";
@@ -18,7 +17,7 @@ import NotFound from "./screens/NotFound.jsx";
 import { listProducts } from "./Redux/Actions/ProductActions.js";
 import { listOrders } from "./Redux/Actions/OrderActions.js";
 import Register from "./screens/Register.jsx";
-import PrivateRouter from "../PrivateRouter.js";
+import PrivateRouter from "./PrivateRouter.jsx";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,25 +32,46 @@ const App = () => {
   }, [dispatch, userInfo]);
 
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <PrivateRouter path="/" element={<HomeScreen />} />
-          <PrivateRouter path="/products" element={<ProductScreen />} />
-          <PrivateRouter path="/register" element={<Register />} />
-          <PrivateRouter path="/orders" element={<OrderScreen />} />
-          <PrivateRouter path="/order/:id" element={<OrderDetailScreen />} />
-          <PrivateRouter path="/addproduct" element={<AddProduct />} />
-          <PrivateRouter path="/users" element={<UsersScreen />} />
-          <PrivateRouter
-            path="/product/:id/edit"
-            element={<ProductEditScreen />}
-          />
-          <Route path="/login" element={<Login />} />
-          <PrivateRouter path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route exact path="/" element={<PrivateRouter />}>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Route> */}
+        {/* <PrivateRouter path="/" element={<HomeScreen />} /> */}
+        <Route path="/" element={<PrivateRouter element={<HomeScreen />} />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/products"
+          element={<PrivateRouter element={<ProductScreen />} />}
+        />
+        <Route
+          path="/register"
+          element={<PrivateRouter element={<Register />} />}
+        />
+        <Route
+          path="/orders"
+          element={<PrivateRouter element={<OrderScreen />} />}
+        />
+        <Route
+          path="/order/:id"
+          element={<PrivateRouter element={<OrderDetailScreen />} />}
+        />
+        <Route
+          path="/addproduct"
+          element={<PrivateRouter element={<AddProduct />} />}
+        />
+        <Route
+          path="/users"
+          element={<PrivateRouter element={<UsersScreen />} />}
+        />
+        <Route
+          path="/product/:id/edit"
+          element={<PrivateRouter element={<ProductEditScreen />} />}
+        />
+
+        <Route path="*" element={<PrivateRouter element={<NotFound />} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/PrivateRouter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Navigate } from "react-router-dom";
+
+const PrivateRouter = ({ element: Element, ...rest }) => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  if (!userInfo || !userInfo.isAdmin) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Route {...rest} element={<Element />} />;
+};
+
+export default PrivateRouter;
